6a4a068700f7f9e37ee2051f26b35e7d37480590
Json Lee <lijiansong@ict.ac.cn>
2019-07-25 09:57:18 +0800

    automatically

---
+++ cpp/arg-parse/wrapper.h
@@ -0,0 +1,9 @@
+#ifndef __WRAPPER_H__
+#define __WRAPPER_H__
+
+#include "cxxopts/include/cxxopts.hpp"
+
+using ParseResult = cxxopts::ParseResult;
+using Options = cxxopts::Options;
+
+#endif // __WRAPPER_H__
