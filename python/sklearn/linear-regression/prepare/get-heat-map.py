fab9b80df720e4a274f82a39e2b9bd96f8a27607
Json Lee <lijiansong@ict.ac.cn>
2019-11-04 08:52:59 +0800

    post process

--- python/sklearn/linear-regression/prepare/get-heat-map.py
+++ python/sklearn/linear-regression/prepare/get-heat-map.py
@@ -264,9 +264,116 @@ def get_bs_vs_mp(in_file_name, out_file_name):
             file_reader.close()
             file_writer.close()
 
+def get_tn_vs_dp(in_file_name, out_file_name):
+    '''
+    Note: best case for dense int8 new log is:
+        batch_size  data_parallel   model_parallel   thread_num
+        16	4	1	8
+
+    batch size:     1 2 4 8 16 32 64 128 256 512 1024
+    data parallel:  1 2 4 8 16 32
+    model parallel: 1 2 4 8 16 32
+    thread num:     1 2 4 8 16 32 64 128
+
+    for 'batch size' vs 'thread num': 11 x 8
+    '''
+    best_bs, best_dp, best_mp, best_tn = 16, 4, 1, 8
+    batch_size_index_dict={1:1, 2:2, 4:3, 8:4, 16:5, 32:6, 64:7, 128:8, 256:9, 512:10, 1024:11}
+    thread_num_index_dict={1:1, 2:2, 4:3, 8:4, 16:5, 32:6, 64:7, 128:8}
+    data_parallel_index_dict={1:1, 2:2, 4:3, 8:4, 16:5, 32:6}
+    model_parallel_index_dict={1:1, 2:2, 4:3, 8:4, 16:5, 32:6}
+    file_reader = open(in_file_name, 'r')
+    file_writer = open(out_file_name, 'w')
+    res_list = [None for _ in range(len(thread_num_index_dict)*len(data_parallel_index_dict))]
+    try:
+        text_lines = file_reader.readlines()
+        for line in text_lines:
+            line = line.rstrip('\n')
+            line = line.split('\t')
+            batch_size, data_parallel, model_parallel, thread_num, fifo_size, end2end_fps = int(line[0]), int(line[1]), int(line[2]), int(line[3]), int(line[4]), float(line[5])
+            if model_parallel == best_mp and batch_size == best_bs:
+                index = (thread_num_index_dict[thread_num] - 1) * len(data_parallel_index_dict) + data_parallel_index_dict[data_parallel] - 1
+                res_list[index] = end2end_fps
+        print(len(res_list), res_list)
+        for i in range(len(thread_num_index_dict)):
+            res_str = ''
+            for j in range(len(data_parallel_index_dict)):
+                index = i * len(data_parallel_index_dict) + j
+                if j == len(data_parallel_index_dict) - 1:
+                    if res_list[index] == None:
+                        res_str += '\n'
+                    else:
+                        res_str += str(res_list[index]) + '\n'
+                else:
+                    if res_list[index] == None:
+                        res_str += ','
+                    else:
+                        res_str += str(res_list[index]) + ','
+            print(res_str)
+            file_writer.write(res_str)
+
+    finally:
+        if file_reader:
+            file_reader.close()
+            file_writer.close()
+
+def get_tn_vs_mp(in_file_name, out_file_name):
+    '''
+    Note: best case for dense int8 new log is:
+        batch_size  data_parallel   model_parallel   thread_num
+        16	4	1	8
+
+    batch size:     1 2 4 8 16 32 64 128 256 512 1024
+    data parallel:  1 2 4 8 16 32
+    model parallel: 1 2 4 8 16 32
+    thread num:     1 2 4 8 16 32 64 128
+
+    for 'batch size' vs 'thread num': 11 x 8
+    '''
+    best_bs, best_dp, best_mp, best_tn = 16, 4, 1, 8
+    batch_size_index_dict={1:1, 2:2, 4:3, 8:4, 16:5, 32:6, 64:7, 128:8, 256:9, 512:10, 1024:11}
+    thread_num_index_dict={1:1, 2:2, 4:3, 8:4, 16:5, 32:6, 64:7, 128:8}
+    data_parallel_index_dict={1:1, 2:2, 4:3, 8:4, 16:5, 32:6}
+    model_parallel_index_dict={1:1, 2:2, 4:3, 8:4, 16:5, 32:6}
+    file_reader = open(in_file_name, 'r')
+    file_writer = open(out_file_name, 'w')
+    res_list = [None for _ in range(len(thread_num_index_dict)*len(model_parallel_index_dict))]
+    try:
+        text_lines = file_reader.readlines()
+        for line in text_lines:
+            line = line.rstrip('\n')
+            line = line.split('\t')
+            batch_size, data_parallel, model_parallel, thread_num, fifo_size, end2end_fps = int(line[0]), int(line[1]), int(line[2]), int(line[3]), int(line[4]), float(line[5])
+            if model_parallel == best_mp and batch_size == best_bs:
+                index = (thread_num_index_dict[thread_num] - 1) * len(model_parallel_index_dict) + model_parallel_index_dict[model_parallel] - 1
+                res_list[index] = end2end_fps
+        print(len(res_list), res_list)
+        for i in range(len(thread_num_index_dict)):
+            res_str = ''
+            for j in range(len(model_parallel_index_dict)):
+                index = i * len(model_parallel_index_dict) + j
+                if j == len(model_parallel_index_dict) - 1:
+                    if res_list[index] == None:
+                        res_str += '\n'
+                    else:
+                        res_str += str(res_list[index]) + '\n'
+                else:
+                    if res_list[index] == None:
+                        res_str += ','
+                    else:
+                        res_str += str(res_list[index]) + ','
+            print(res_str)
+            file_writer.write(res_str)
+
+    finally:
+        if file_reader:
+            file_reader.close()
+            file_writer.close()
 if __name__ == '__main__':
     print('===---------------- dense int8 ----------------===')
     get_bs_vs_tn('int8-dense-191031.txt', 'bs_tn.txt')
-    get_dp_vs_mp('int8-dense-191031.txt', 'dp_mp.txt')
     get_bs_vs_dp('int8-dense-191031.txt', 'bs_dp.txt')
     get_bs_vs_mp('int8-dense-191031.txt', 'bs_mp.txt')
+    get_tn_vs_dp('int8-dense-191031.txt', 'tn_dp.txt')
+    get_tn_vs_mp('int8-dense-191031.txt', 'tn_mp.txt')
+    get_dp_vs_mp('int8-dense-191031.txt', 'dp_mp.txt')
--- python/sklearn/linear-regression/prepare/heat-map-plot.py
+++ python/sklearn/linear-regression/prepare/heat-map-plot.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 import csv
 import codecs
 
-def plot_heatmap(file_name, xlabel_name, ylabel_name, xtick_labels, ytick_labels):
-    file = open(file_name, "rb")
+def plot_heatmap(in_file_name, out_fig_name, xlabel_name, ylabel_name, xtick_labels, ytick_labels):
+    file = open(in_file_name, "rb")
     read_res = csv.reader(codecs.iterdecode(file, 'utf-8'), delimiter=',')
     data = list(read_res)
     print(type(data))
@@ -21,11 +21,11 @@ def plot_heatmap(file_name, xlabel_name, ylabel_name, xtick_labels, ytick_labels
     print(data)
     df = pd.DataFrame(data)
     print("data shape:", df.shape)
-    _, ax = plt.subplots(figsize=(8, 5))
+    _, ax = plt.subplots(figsize=(6, 5))
     #p1 = sns.heatmap(df, cmap='Reds', annot=True)
-    #p1 = sns.heatmap(df, cmap='Reds')
-    fig = sns.heatmap(df, cmap='Reds', ax=ax, cbar_kws={'label': 'End to end FPS'}, linewidths=0.01, linecolor='black')
-    #fig = sns.heatmap(df, cmap='YlGnBu', cbar_kws={'label': 'End to end FPS'}, linewidths=.1)
+    #fig = sns.heatmap(df, cmap='Reds', ax=ax, cbar_kws={'label': 'End to end FPS'}, linewidths=0.01, linecolor='black')
+    fig = sns.heatmap(df, cmap='YlGnBu', ax=ax, cbar_kws={'label': 'End to end FPS'}, linewidths=0.01, linecolor='black')
+    #fig = sns.heatmap(df, cmap='YlGnBu', ax=ax, cbar_kws={'label': 'End to end FPS'}, linewidths=0.0, linecolor='black')
     plt.xlabel(xlabel_name)
     plt.ylabel(ylabel_name)
     fig.set_xticklabels(xtick_labels)
@@ -34,6 +34,7 @@ def plot_heatmap(file_name, xlabel_name, ylabel_name, xtick_labels, ytick_labels
     plt.setp(label_y, rotation=45, horizontalalignment='right')
     label_x = fig.get_xticklabels()
     plt.setp(label_x, rotation=45, horizontalalignment='right')
+    #plt.savefig(out_fig_name)
     plt.show()
 
 if __name__ == '__main__':
@@ -41,8 +42,10 @@ if __name__ == '__main__':
     tn_ticklabels = ['1', '2', '4', '8', '16', '32', '64', '128']
     mp_ticklabels = ['1', '2', '4', '8', '16', '32']
     dp_ticklabels = ['1', '2', '4', '8', '16', '32']
-    plot_heatmap('bs_mp.txt', 'model parallelism', 'batch size', mp_ticklabels, bs_ticklabels)
-    plot_heatmap('dp_mp.txt', 'model parallelism', 'data parallelism', mp_ticklabels, dp_ticklabels)
-    plot_heatmap('bs_dp.txt', 'data parallelism', 'batch size', dp_ticklabels, bs_ticklabels)
-    plot_heatmap('bs_tn.txt', 'thread number', 'batch size', tn_ticklabels, bs_ticklabels)
+    plot_heatmap('bs_mp.txt', 'bs_mp.png', 'model parallelism', 'batch size', mp_ticklabels, bs_ticklabels)
+    plot_heatmap('dp_mp.txt', 'dp_mp.png', 'model parallelism', 'data parallelism', mp_ticklabels, dp_ticklabels)
+    plot_heatmap('bs_dp.txt', 'bs_dp.png', 'data parallelism', 'batch size', dp_ticklabels, bs_ticklabels)
+    plot_heatmap('bs_tn.txt', 'bs_tn.png', 'thread number', 'batch size', tn_ticklabels, bs_ticklabels)
+    plot_heatmap('tn_dp.txt', 'tn_dp.png', 'thread number', 'data parallelism', tn_ticklabels, dp_ticklabels)
+    plot_heatmap('tn_mp.txt', 'tn_mp.png', 'thread number', 'model parallelism', tn_ticklabels, mp_ticklabels)
 
