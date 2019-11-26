e28b9b6c750ba4063297baf8a73e0a35ceea52e5
Json Lee <lijiansong@ict.ac.cn>
2019-11-26 13:14:30 +0800

    shell has no built-in 'map' data structure

--- shell/map.sh
+++ shell/map.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Note: shell has no built-in map data structure
 
 declare line_map=(["100"]=1 ["200"]=2)
 line_map["300"]="3"
@@ -12,24 +13,24 @@ do
     echo ${line_map[${key}]}
 done
 
-network_list=(
-     alexnet
-     googlenet
-     inception-v3
-     mobilenet
-     resnet101
-     resnet152
-     resnet18
-     resnet34
-     resnet50
-     squeezenet
-     vgg16
-     vgg19
+batchsize_lineno_map=(
+"alexnet|4"
+"googlenet|4"
+"inception-v3|8"
+"mobilenet|8"
+"resnet101|4"
+"resnet152|4"
+"resnet18|4"
+"resnet34|4"
+"resnet50|4"
+"squeezenet|11"
+"vgg16|4"
+"vgg19|4"
 )
-declare batchsize_lineno_map=(["alexnet"]=4 ["googlenet"]=4 ["inception-v3"]=8 ["mobilenet"]=8 ["resnet101"]=4 ["resnet152"]=4 ["resnet18"]=4 ["resnet34"]=4 ["resnet50"]=4 ["squeezenet"]=11 ["vgg16"]=4 ["vgg19"]=4)
 
-for net in ${network_list[@]}; do
-    echo ${net}
-    echo ${batchsize_lineno_map[${net}]}
+for item in ${batchsize_lineno_map[@]}; do
+    net=$(echo "${item}"|awk -F "|" '{print $1}')
+    line_no=$(echo "${item}"|awk -F "|" '{print $2}')
+    echo "net: ${net}, line_no: ${line_no}"
 done
 
