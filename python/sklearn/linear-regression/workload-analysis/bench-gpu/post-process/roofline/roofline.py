d7f7419fcc8b3f1c2f81c7a883a7e816ce1c3979
Json Lee <lijiansong@ict.ac.cn>
2020-03-22 01:24:38 +0800

    titan xp and tesla k40m op roofline

--- python/sklearn/linear-regression/workload-analysis/bench-gpu/post-process/roofline/roofline.py
+++ python/sklearn/linear-regression/workload-analysis/bench-gpu/post-process/roofline/roofline.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 import seaborn as sns;
 
-def get_data(data_file_path, debug=True):
+def extract_model_data(data_file_path, debug=True):
     data_file_reader = open(data_file_path, 'r')
     # key is net name, value is list of <batch size, gflops, intensity> tuples
     gflops_intensity_dict = {}
@@ -38,6 +38,22 @@ def get_data(data_file_path, debug=True):
         data_file_reader.close()
     return gflops_intensity_dict
 
+def extract_op_data(data_file_path, debug=True):
+    data_file_reader = open(data_file_path, 'r')
+    # key is net name, value is list of <batch size, gflops, intensity> tuples
+    op_data_list = []
+    try:
+        text_lines = data_file_reader.readlines()
+        for line in text_lines:
+            line = line.rstrip('\n')
+            _, op_type, glops, intensity = line.split('\t')
+            op_data_list.append((op_type, float(glops), float(intensity)))
+        if debug:
+            print(op_data_list)
+    finally:
+        data_file_reader.close()
+    return op_data_list
+
 # find out the max intensity and min gflops
 def find_boundard_pairs(gflops_intensity_dict):
     max_intensity = -1
@@ -48,7 +64,7 @@ def find_boundard_pairs(gflops_intensity_dict):
             min_flops = min(min_flops, gflops)
     return max_intensity, min_flops
 
-def draw_roofline(gflops_intensity_dict, peak_flops, peak_membdw):
+def draw_model_roofline(gflops_intensity_dict, peak_flops, peak_membdw):
     # set color palette for different dnns
     net_type_set = {k for k in gflops_intensity_dict}
     colors = sns.color_palette("hls", n_colors=len(net_type_set) + 2)
@@ -131,14 +147,81 @@ def draw_roofline(gflops_intensity_dict, peak_flops, peak_membdw):
 
     plt.show()
 
+def draw_op_roofline(op_data_list, peak_flops, peak_membdw):
+    op_type_set = {record[0] for record in op_data_list}
+    colors = sns.color_palette("hls", n_colors=len(op_type_set) + 2)
+    layer_color_map = {val:i for i, val in enumerate(list(op_type_set))}
+    #print(layer_color_map)
+    fig, ax = plt.subplots(figsize=(6, 6))
+
+    # 1. plot the <flops, intensity> pairs
+    for i in op_data_list:
+        op_type, flops, intensity = str(i[0]), i[1], i[2]
+        if op_type == 'Convolution':
+            ax.plot(intensity, flops, 'x',
+                    color=colors[layer_color_map[op_type]], label=op_type, marker='x')
+        elif op_type == 'InnerProduct':
+            ax.plot(intensity, flops, 'v',
+                    color=colors[layer_color_map[op_type]], label=op_type, marker='v')
+        elif op_type == 'Pooling':
+            ax.plot(intensity, flops, '*',
+                    color=colors[layer_color_map[op_type]], label=op_type, marker='*')
+        elif op_type == 'Scale':
+            ax.plot(intensity, flops, 's',
+                    color=colors[layer_color_map[op_type]], label=op_type, marker='s')
+        elif op_type == 'Eltwise':
+            ax.plot(intensity, flops, 'd',
+                    color=colors[layer_color_map[op_type]], label=op_type, marker='d')
+        elif op_type == 'ReLU':
+            ax.plot(intensity, flops, 'p',
+                    color=colors[layer_color_map[op_type]], label=op_type, marker='p')
+        elif op_type == 'BatchNorm':
+            ax.plot(intensity, flops, 'o',
+                    color=colors[layer_color_map[op_type]], label=op_type, marker='o')
+        elif op_type == 'Softmax':
+            ax.plot(intensity, flops, '+',
+                    color=colors[layer_color_map[op_type]], label=op_type, marker='+')
+        elif op_type == 'LRN':
+            ax.plot(intensity, flops, '^',
+                    color=colors[layer_color_map[op_type]], label=op_type, marker='^')
+
+    # 2. plot the roof line
+    x1 = peak_flops / peak_membdw
+    y1 = peak_flops
+    max_op_intensity = max([i[2] for i in op_data_list])
+    ax.hlines(y=y1, xmin=x1,
+        xmax=max_op_intensity+15, linewidth=1.5, color='red')
+    min_flops = min([i[1] for i in op_data_list])
+    x2 = min_flops / peak_membdw
+    y2 = peak_membdw * x2
+    ax.plot([x1, x2], [y1, y2], linewidth=1.5, color='red')
+
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+    #plt.xscale('log', basex=2)
+    #plt.yscale('log', basey=2)
+    ax.set_ylabel('GFLOps/sec', fontsize=10)
+    ax.set_xlabel('Operational Intensity (FLOps/Byte)', fontsize=10)
+
+    handles, labels = ax.get_legend_handles_labels()
+    #print(labels)
+    labels_od = OrderedDict(zip(labels, handles))
+    ax.legend(labels_od.values(), labels_od.keys(), loc='upper left')
+
+    plt.show()
+
 if __name__ == '__main__':
     # Titan Xp 12.15 TFLOPS, 547.7 GB/s
     # Tesla K40m 5.046 TFLOPS, 288.4 GB/s
-    titan_data = get_data('titan_xp_data_throughput.txt')
+    titan_model_data = extract_model_data('titan_xp_model_throughput.txt')
     titan_peak_flops = 12.15*1000
     titan_peak_mem_bandwidth = 547.7
-    draw_roofline(titan_data, titan_peak_flops, titan_peak_mem_bandwidth)
-    k40m_data = get_data('tesla_k40m_data_throughput.txt')
+    #draw_model_roofline(titan_model_data, titan_peak_flops, titan_peak_mem_bandwidth)
+    k40m_model_data = extract_model_data('tesla_k40m_model_throughput.txt')
     k40m_peak_flops = 5.046*1000
     k40m_peak_mem_bandwidth = 288.4
-    draw_roofline(k40m_data, titan_peak_flops, titan_peak_mem_bandwidth)
+    #draw_model_roofline(k40m_model_data, titan_peak_flops, titan_peak_mem_bandwidth)
+    titan_op_data = extract_op_data('titan_xp_op_throughput.txt')
+    draw_op_roofline(titan_op_data, titan_peak_flops, titan_peak_mem_bandwidth)
+    k40m_op_data = extract_op_data('tesla_k40m_op_throughput.txt')
+    draw_op_roofline(k40m_op_data, k40m_peak_flops, k40m_peak_mem_bandwidth)
