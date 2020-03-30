8468ec5d2b74490ff1238ebcd00eb93a16804f00
Json Lee <lijiansong@ict.ac.cn>
2020-03-31 00:44:15 +0800

    How Lambdas Differ, Implementation-wise, from Plain Functions and Functor classes？

--- cpp/lambda-expr/lambda-diff-with-plain-functor/capture-by-reference.cpp
+++ cpp/lambda-expr/lambda-diff-with-plain-functor/capture-by-reference.cpp
@@ -1,17 +1,18 @@
 class Functor {
 public:
-  Functor(const int x) : m_x(x) {}
+  Functor(int &x) : m_x(x) {}
 
-  int operator()(int a) { return a + m_x; }
+  int operator()(int a) { return a + m_x++; }
 
 private:
-  int m_x;
+  int &m_x;
 };
 
 int main() {
   int x = 3;
 
-  auto lambda = [=](int a) { return a + x; };
+  auto lambda = [&](int a) { return a + x++; };
+
   Functor functor(x);
 
   volatile int y1 = functor(5);
