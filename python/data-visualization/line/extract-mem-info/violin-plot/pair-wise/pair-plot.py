9f5bb26a5d9ec7606a6bb69897b4dd742de47ac7
Json Lee <lijiansong@ict.ac.cn>
2021-04-07 11:36:26 +0800

    ml mem logs

--- python/data-visualization/line/extract-mem-info/violin-plot/pair-wise/pair-plot.py
+++ python/data-visualization/line/extract-mem-info/violin-plot/pair-wise/pair-plot.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 from collections import OrderedDict
+import seaborn as sns
 
-def extract_plot(data_file_blob='data.txt'):
+def extract_plot(data_file_blob='data.txt', outlier=100.0):
     # 1. extract data.
     access_time_interval_list, blk_size_list = [], []
     with open(data_file_blob, 'r') as f:
@@ -11,7 +12,12 @@ def extract_plot(data_file_blob='data.txt'):
             items = data.split(',')
             _ati, _size = float(items[0]), float(items[1])
             access_time_interval_list.append(_ati)
-            blk_size_list.append(_size/1000.0)
+            # FIXME: only scale for GBDT in tf internal logs
+            if 'gbdt' in data_file_blob:
+                blk_size_list.append(_size/100.0)
+            else:
+                blk_size_list.append(_size/1000.0)
+
     # 2. scatter plot.
     fig, ax1 = plt.subplots()
     y1_color = 'tab:red'
@@ -33,5 +39,14 @@ def extract_plot(data_file_blob='data.txt'):
     fig.tight_layout()
     plt.show()
 
+    # violin plot
+    valid_ati_list = list(filter(lambda x: x < outlier, access_time_interval_list))
+    ax = sns.violinplot(x=valid_ati_list, orient='h')
+    ax.set_xlabel('Access time interval(/us)')
+    plt.show()
+
 if __name__ == '__main__':
-    extract_plot('knn-data.txt')
+    #extract_plot('knn-data.txt', 100.0)
+    #extract_plot('kmeans-data.txt', 80.0)
+    #extract_plot('log-regr-data.txt', 50.0)
+    extract_plot('gbdt-data.txt', 100.0)
